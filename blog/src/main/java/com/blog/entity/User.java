7ba8c6928a554b5b1ce2820ab395f4708a25984a
ASL <ASL@DESKTOP-FMIS25R>
2022-06-27 02:28:07 +0600

    2nd

---
+++ blog/src/main/java/com/blog/entity/User.java
@@ -0,0 +1,28 @@
+package com.blog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String about;
+
+}
